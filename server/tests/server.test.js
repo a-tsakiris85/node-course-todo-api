ddf8c6fa34ecc1545eea5c42cf83c068ee390dfd
art85 <art85@cornell.edu>
2018-07-13 22:05:06 -0400

    add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,26 @@ const request = require('supertest');
 const {app} = require('./../server'); //to go backwards!
 const {Todo} = require('./../models/todo');
 
+const test_todos = [
+  {text: "first test todo"},
+  {text: "second test todo"},
+  {text: "third test todo"}
+];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(test_todos);
+  }).then(() => done());
 });
 
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos').expect(200).expect((res) => {
+      expect(res.body.todos.length).toBe(test_todos.length)
+    })
+    .end(done); //nothing async to check at the end. So nothing special needed.
+  });
+});
 describe('POST /todos', () => {
   it('should create new todo', (done) => { //mocha async
     let text = 'this is a test todo text';
@@ -16,12 +32,12 @@ describe('POST /todos', () => {
       .expect((res) => {
         expect(res.body.text).toBe(text); //a custom assertion
       })
-      .end((err, res) => {
+      .end((err, res) => { //this is if we have some async stuff
         if(err) {
           return done(err);
         }
 
-        Todo.find().then((todos) => { //actually check the database
+        Todo.find({text}).then((todos) => { //actually check the database
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -39,7 +55,7 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(test_todos.length);
           done();
         }).catch((err) => {
           done(e);
