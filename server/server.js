b6b8b51901111eae38de04a46691c170927bc996
art85 <art85@cornell.edu>
2018-07-14 23:01:07 -0400

    fixed heroku setup

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.listen(port, () => { //local port, or we can do Heroku
-  console.log("Started on port ", port;
+  console.log("Started on port ", port);
 });
 
 module.exports = {
