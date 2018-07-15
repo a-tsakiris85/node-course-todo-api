e3b8c60f629bf33be23e5d84b97509c87faf2a6c
art85 <art85@cornell.edu>
2018-07-14 23:59:32 -0400

    add DELETE todos/:id route

--- server/server.js
+++ server/server.js
@@ -52,6 +52,22 @@ app.get('/todos/:id', (req, res) => {
   }
 })
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  else {
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo})
+    }, (err) => {
+      res.status(400).send();
+    });
+  }
+})
 app.listen(port, () => { //local port, or we can do Heroku
   console.log("Started on port ", port);
 });
