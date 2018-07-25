666be9ec6f823085b0a8366a6f018e9faea8c60b
art85 <art85@cornell.edu>
2018-07-25 17:37:53 -0400

    update config to use private json file

--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ var UserSchema = new mongoose.Schema({ //automatically turns Name -> names colle
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens = user.tokens.concat([{access, token}]);
   return user.save().then(() => { //return a promise because async.
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     //return new Promise((resolve, reject) => reject()); //always rejects
     return Promise.reject();
